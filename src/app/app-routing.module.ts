d2c3e514d554d4a560c50534ab91e54053363974
JossueN <jozz1995.jm@gmail.com>
2024-05-27 01:35:49 -0600

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
+import { DashboardComponent } from './vistas/dashboard/dashboard.component';
+import { EditarComponent } from './vistas/editar/editar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./vistas/login/login.component";
+import { NuevoComponent } from "./vistas/nuevo/nuevo.component";
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login', pathMatch: 'full' }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'nuevo', component: NuevoComponent },
+  { path: 'editar', component: EditarComponent}
+ 
 ];
 
 @NgModule({
@@ -13,3 +19,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ LoginComponent,DashboardComponent,NuevoComponent,EditarComponent];
